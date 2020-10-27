6fa6c852d14f4b626e45c41800621d7d6b3036b3
Artem Bordetskiy <abordetskiy@gmail.com>
2020-10-27 15:49:00 -0500

    feat: add overlay object(empty) and layer control

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,5 @@
-// Define base layer for mapbox map
-var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+// Define base layer
+var streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
@@ -7,6 +7,24 @@ var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
   id: "mapbox/streets-v11",
   accessToken: API_KEY
 });
+ // Define dark layer
+var darkMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+  });
+
+
+// Define a baseMaps object to hold layers
+var baseMaps = {
+  "Street Map": streetMap,
+  "Dark Map": darkMap
+};
+
+  // Create overlay object
+  var overlayMaps = {
+  };
 
 // Establish Map variable in 'map' tag in index.html
 var myMap = L.map("map", {
@@ -14,5 +32,10 @@ var myMap = L.map("map", {
     39.8283, -98.5795
   ],
   zoom: 4,
-  layers: streetmap
+  layers: streetMap
 });
+
+  // Create a layer control & add to map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
