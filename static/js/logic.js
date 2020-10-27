04f6c14759493abebec244d83ff061c9b71504f9
Artem Bordetskiy <abordetskiy@gmail.com>
2020-10-27 15:50:54 -0500

    feat: add satellite layer

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,13 @@
-// Define base layer
+
+// Define satellite layer
+var satelliteMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.satellite",
+  accessToken: API_KEY
+});
+
+// Define street layer
 var streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -7,6 +16,7 @@ var streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
   id: "mapbox/streets-v11",
   accessToken: API_KEY
 });
+
  // Define dark layer
 var darkMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -14,7 +24,6 @@ var darkMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z
     id: "dark-v10",
     accessToken: API_KEY
   });
-
 
 // Define a baseMaps object to hold layers
 var baseMaps = {
@@ -32,7 +41,7 @@ var myMap = L.map("map", {
     39.8283, -98.5795
   ],
   zoom: 4,
-  layers: streetMap
+  layers: satelliteMap
 });
 
   // Create a layer control & add to map
